5e60cdfe0cff97b19792167b664b4f415752bb04
Jia <ji3g4m6zo6@msn.com>
2019-05-01 22:22:17 +0800

    Init

--- PDFTest/ViewController.swift
+++ PDFTest/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import PDFKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, PDFViewDelegate {
+    @IBOutlet var pdfView: PDFView!
+    var savedDestinationPoint = CGPoint()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        pdfView.delegate = self
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerObserver), userInfo: nil, repeats: true)
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if let path = Bundle.main.path(forResource: "sample", ofType: "pdf") {
+            if let pdfDocument = PDFDocument(url: URL(fileURLWithPath: path)) {
+                pdfView.document = pdfDocument
+            }
+        }
+    }
+    
+    @objc func timerObserver() {
+//        print(pdfView.currentDestination!.point)
+//        print(pdfView.scaleFactor)
+//        print(pdfView.currentPage!)
+//        print(pdfView.documentView!.bounds)
+    }
+    
+    @IBAction func setDirectionPoint(_ sender: Any) {
+        pdfView.go(to: PDFDestination(page: pdfView.document!.page(at: 0)!, at: savedDestinationPoint))
+        //        pdfView.go(to: pdfView.document!.page(at: 0)!)
+        print(pdfView.currentPage!)
+        print(savedDestinationPoint)
+        print(pdfView.currentDestination!.point)
+    }
+    
+    @IBAction func getDirectionPoint(_ sender: Any) {
+        savedDestinationPoint = pdfView.currentDestination!.point
+    }
 }
 
